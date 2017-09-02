d8b5cc1bd3094f4591b1d76ddc30694cdecd4792
Tom Charter <tbc1g14@soton.ac.uk>
2017-09-02 20:35:55 +0100

    removing comments

--- main.py
+++ main.py
@@ -11,11 +11,9 @@ img1 = img.filter(ImageFilter.GaussianBlur(radius=5))
 
 
 draw = ImageDraw.Draw(img1,'RGBA')
-# font = ImageFont.truetype(<font-file>, <font-size>)
 font = ImageFont.truetype("/usr/share/fonts/TTF/DejaVuSansCondensed-Bold.ttf", 72)
 font2= ImageFont.truetype("/usr/share/fonts/TTF/DejaVuSansCondensed-Bold.ttf", 36)
 draw.rectangle([0,300,1920,430],(0,0,0,127),(0,0,0,127))
-# draw.text((x, y),"Sample Text",(r,g,b))
 draw.text((0, 300),"Name",(255,255,255),font=font)
 draw.text((0, 372),"Contact Informatoing",(255,255,255),font=font2)
 img1.save("/tmp/tmp_screenshot1.png")
