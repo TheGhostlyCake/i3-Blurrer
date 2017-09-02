90dfd490f68c3194b467e091e3f138ece32d40cd
Andrew Barrett-Sprot <abarrettsprot@gmail.com>
2017-09-02 15:35:17 -0700

    use mss for screenshots

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-#!/bin/python
-
-import os
-from PIL import ImageFilter, Image, ImageDraw, ImageFont
-from PIL import Image
+from mss import mss
+from PIL import ImageFilter, ImageDraw, ImageFont, Image
 
 
-os.system("scrot /tmp/tmp_screenshot.png")
-img = Image.open("/tmp/tmp_screenshot.png")
-img1 = img.filter(ImageFilter.GaussianBlur(radius=5))
+def main():
+    with mss() as sct:
+        # grab screenshot from monitor 0
+        sct_img = sct.grab(sct.monitors[0])
+        im = Image.frombytes('RGB', sct_img.size, sct_img.rgb)
+
+        img1 = im.filter(ImageFilter.GaussianBlur(radius=5))
+
+        draw = ImageDraw.Draw(img1, 'RGB')
+        font = ImageFont.truetype("/usr/share/fonts/TTF/DejaVuSansCondensed-Bold.ttf", 72)
+        font2 = ImageFont.truetype("/usr/share/fonts/TTF/DejaVuSansCondensed-Bold.ttf", 36)
+        draw.rectangle([0, 300, 1920, 430], (0, 0, 0, 127), (0, 0, 0, 127))
+        draw.text((0, 300), "Name", (255, 255, 255), font=font)
+        draw.text((0, 372), "Contact Information", (255, 255, 255), font=font2)
+        img1.save("/tmp/tmp_screenshot1.png")
 
 
-draw = ImageDraw.Draw(img1,'RGBA')
-font = ImageFont.truetype("/usr/share/fonts/TTF/DejaVuSansCondensed-Bold.ttf", 72)
-font2= ImageFont.truetype("/usr/share/fonts/TTF/DejaVuSansCondensed-Bold.ttf", 36)
-draw.rectangle([0,300,1920,430],(0,0,0,127),(0,0,0,127))
-draw.text((0, 300),"Name",(255,255,255),font=font)
-draw.text((0, 372),"Contact Informatoing",(255,255,255),font=font2)
-img1.save("/tmp/tmp_screenshot1.png")
+if __name__ == "__main__":
+    main()
