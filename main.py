2294535d027fdaf02e83f9becff525e3dd90e170
Tom Charter <tbc1g14@soton.ac.uk>
2017-09-03 07:14:29 +0100

    Making changes before merging PR

--- main.py
+++ main.py
@@ -15,5 +15,5 @@ font = ImageFont.truetype("/usr/share/fonts/TTF/DejaVuSansCondensed-Bold.ttf", 7
 font2= ImageFont.truetype("/usr/share/fonts/TTF/DejaVuSansCondensed-Bold.ttf", 36)
 draw.rectangle([0,300,1920,430],(0,0,0,127),(0,0,0,127))
 draw.text((0, 300),"Name",(255,255,255),font=font)
-draw.text((0, 372),"Contact Informatoing",(255,255,255),font=font2)
+draw.text((0, 372),"Contact Information",(255,255,255),font=font2)
 img1.save("/tmp/tmp_screenshot1.png")
